464119045ae3269236ec867dfe41f00abe145a60
잉퓨 <dayeon620@kakao.com>
2022-08-19 03:25:01 +0900

    write UserService

--- src/main/java/org/cau/shoppingmall/service/UserService.java
+++ src/main/java/org/cau/shoppingmall/service/UserService.java
@@ -3,8 +3,42 @@ package org.cau.shoppingmall.service;
 public interface UserService {
 
     /*
-    *
-    * */
+    User create : 사용자에게 입력받은 유저 정보를 생성한다
+    프로세스 로직:
+    -user entity 생성하기
+
+    점검사항:
+    -아이디 중복 체크하기
+
+    */
+
+    /*
+    User get : Id에 해당하는 user 정보를 반환한다
+    프로세스 로직:
+    -session에서 ID 확인 후 반환
+
+    */
+
+    /*
+    User update : 사용자가 수정한 유저 정보를 반영한다
+    프로세스 로직:
+    - 인자로 넘어온 내용을 User 정보에 반영한다
+
+    DB 변경사항:
+    -User entity에 해당하는 내용 변경
+
+    */
+
+
+    /*
+    User delete : 사용자에게 삭제 요청을 받은 유저 정보를 삭제한다
+    프로세스 로직:
+    - 요청이 들어온 User의 id에 해당하는 DB 삭제
+
+    DB 변경사항:
+    -
+
+    */
 
 
 }
