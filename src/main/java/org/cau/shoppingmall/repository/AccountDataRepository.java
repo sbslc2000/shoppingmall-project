82d09e40b1a8042e3c430dcc28db202cc38c73fd
sbslc2000 <sbslc2000@gmail.com>
2022-08-23 13:22:56 +0900

    conflict 해결 중 ...

--- src/main/java/org/cau/shoppingmall/repository/AccountDataRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cau.shoppingmall.repository;
-
-import org.cau.shoppingmall.entity.user.AccountData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountDataRepository extends JpaRepository<AccountData,Long> {
-}
--- src/main/java/org/cau/shoppingmall/repository/ShoppingDataRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cau.shoppingmall.repository;
-
-import org.cau.shoppingmall.entity.user.ShoppingData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingDataRepository extends JpaRepository<ShoppingData,Long> {
-}
