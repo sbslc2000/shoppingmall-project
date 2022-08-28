7a1410744cf60b235db0cad9552f3c8dc5b7ad09
lims00 <97951581+lims00@users.noreply.github.com>
2022-08-28 18:40:07 +0900

    Add files via upload

---
+++ src/main/resources/static/loginpage/login.js
@@ -0,0 +1,5 @@
+function loginalret() {
+    if (document.getElementById('id').value == "a" || document.getElementById('pw').value == "a") {
+        document.getElementById('loginalret').style.display = 'block';
+    }
+}
