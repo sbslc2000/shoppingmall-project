bf9df53d069d5f43abb599cf28adb8ed56d93b8c
lims00 <97951581+lims00@users.noreply.github.com>
2022-09-05 01:11:46 +0900

    mypage 업로드합니다

---
+++ src/main/resources/static/mypage/mypage_js.js
@@ -0,0 +1,65 @@
+function likebtn1() {
+
+    if (document.getElementById('likebutton1').style.backgroundColor == 'pink') {
+        document.getElementById('likebutton1').style.backgroundColor = 'white';
+        document.getElementById('likebutton1').style.color = 'pink';
+    } else {
+        document.getElementById('likebutton1').style.backgroundColor = 'pink';
+        document.getElementById('likebutton1').style.color = 'white';
+    }
+}
+
+function likebtn2() {
+    2
+    if (document.getElementById('likebutton2').style.backgroundColor == 'pink') {
+        document.getElementById('likebutton2').style.backgroundColor = 'white';
+        document.getElementById('likebutton2').style.color = 'pink';
+    } else {
+        document.getElementById('likebutton2').style.backgroundColor = 'pink';
+        document.getElementById('likebutton2').style.color = 'white';
+    }
+}
+
+function likebtn3() {
+
+    if (document.getElementById('likebutton3').style.backgroundColor == 'pink') {
+        document.getElementById('likebutton3').style.backgroundColor = 'white';
+        document.getElementById('likebutton3').style.color = 'pink';
+    } else {
+        document.getElementById('likebutton3').style.backgroundColor = 'pink';
+        document.getElementById('likebutton3').style.color = 'white';
+    }
+}
+
+function likebtn4() {
+
+    if (document.getElementById('likebutton4').style.backgroundColor == 'pink') {
+        document.getElementById('likebutton4').style.backgroundColor = 'white';
+        document.getElementById('likebutton4').style.color = 'pink';
+    } else {
+        document.getElementById('likebutton4').style.backgroundColor = 'pink';
+        document.getElementById('likebutton4').style.color = 'white';
+    }
+}
+
+function likebtn5() {
+
+    if (document.getElementById('likebutton5').style.backgroundColor == 'pink') {
+        document.getElementById('likebutton5').style.backgroundColor = 'white';
+        document.getElementById('likebutton5').style.color = 'pink';
+    } else {
+        document.getElementById('likebutton5').style.backgroundColor = 'pink';
+        document.getElementById('likebutton5').style.color = 'white';
+    }
+}
+
+function likebtn6() {
+
+    if (document.getElementById('likebutton6').style.backgroundColor == 'pink') {
+        document.getElementById('likebutton6').style.backgroundColor = 'white';
+        document.getElementById('likebutton6').style.color = 'pink';
+    } else {
+        document.getElementById('likebutton6').style.backgroundColor = 'pink';
+        document.getElementById('likebutton6').style.color = 'white';
+    }
+}
