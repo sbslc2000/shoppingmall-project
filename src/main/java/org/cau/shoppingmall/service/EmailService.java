919e81b5661c588b7c67fe3c4cb65e9571acf24d
잉퓨 <dayeon620@kakao.com>
2022-10-04 16:32:30 +0900

    make EmailService

--- src/main/java/org/cau/shoppingmall/service/EmailService.java
+++ src/main/java/org/cau/shoppingmall/service/EmailService.java
@@ -1,5 +1,41 @@
 package org.cau.shoppingmall.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public interface EmailService {
+
+    @Autowired
+    JavaMailSender javaMailSender = null;
+
+    public default void sendMail() {
+
+        // 수신 대상을 담을 ArrayList 생성
+        //ArrayList<String> toUserList = new ArrayList<>();
+
+        // 수신 대상 추가
+        //toUserList.add("수신대상1@gmail.com");
+        //toUserList.add("수신대상2@naver.com");
+
+        // 수신 대상 개수
+        //int toUserSize = toUserList.size();
+
+        // SimpleMailMessage (단순 텍스트 구성 메일 메시지 생성할 때 이용)
+        SimpleMailMessage simpleMessage = new SimpleMailMessage();
+
+        // 수신자 설정
+        //simpleMessage.setTo((String[]) toUserList.toArray(new String[toUserSize]));
+        simpleMessage.setFrom("dayeon620@kakao.com"); //우선 내 메일로......
+
+        // 메일 제목
+        simpleMessage.setSubject("Subject Sample");
+
+        // 메일 내용
+        simpleMessage.setText("Text Sample");
+
+        // 메일 발송
+        javaMailSender.send(simpleMessage);
+    }
 
 }
