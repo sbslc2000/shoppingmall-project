ceb1f650b901ad651deeeb7dee0306c598c39724
sbslc2000 <sbslc2000@gmail.com>
2022-11-02 18:14:13 +0900

    webMvcConfig 에 / 삭제

--- src/main/java/org/cau/shoppingmall/config/WebMvcConfig.java
+++ src/main/java/org/cau/shoppingmall/config/WebMvcConfig.java
@@ -2,21 +2,20 @@ package org.cau.shoppingmall.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-
     @Value("${file.dir}")
     private String fileDir;
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/images/**")
-                .addResourceLocations("file:/"+fileDir);
+                .addResourceLocations("file:"+fileDir);
     }
 
 }
